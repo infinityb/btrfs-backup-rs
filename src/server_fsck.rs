b69f3c2cd2969d20ff3fbeceab4ca8c8ba6d766b
Stacey Ell <stacey.ell@gmail.com>
2014-10-16 08:54:39 -0600

    Use Argparse

--- src/server_fsck.rs
+++ src/server_fsck.rs
@@ -8,15 +8,17 @@ extern crate uuid;
 extern crate msgpack;
 
 extern crate reliable_rw;
+extern crate argparse;
 
+use std::os;
 use std::collections::HashMap;
 use std::collections::hashmap::{Occupied, Vacant};
-use std::os::{args_as_bytes, set_exit_status};
+use std::os::set_exit_status;
 use std::io::fs::stat;
 use std::io::{FileStat, TypeDirectory};
 use repository::{Repository, BackupNode};
 use uuid::Uuid;
-
+use argparse::{ArgumentParser, Store, StoreTrue};
 
 mod repository;
 mod protocol;
@@ -24,30 +26,52 @@ mod btrfs;
 mod crc32;
 
 
-#[cfg(not(test))]
-fn print_usage(program: &[u8]) {
-    let mut stderr = std::io::stderr();
-    let mut out: Vec<u8> = Vec::new();
-    out.extend(b"USAGE: ".iter().map(|x| x.clone()));
-    out.extend(program.iter().map(|x| x.clone()));
-    out.extend(b" repository-directory\n".iter().map(|x| x.clone()));
-    assert!(stderr.write(out.as_slice()).is_ok());
+#[deriving(Show)]
+struct ProgramArgs {
+    respository_path: String,
+    deep: bool,
+    verbose: bool
+}
+
+impl ProgramArgs {
+    fn new() -> ProgramArgs {
+        ProgramArgs {
+            respository_path: "".to_string(),
+            deep: false,
+            verbose: false
+        }
+    }
 }
 
 
 #[cfg(not(test))]
 fn main() {
-    let args_bytes = args_as_bytes();
-    let program_name = args_bytes[0].as_slice().clone();
+    let mut prog_args = ProgramArgs::new();
 
-    if args_bytes.len() < 2 {
-        print_usage(program_name);
-        set_exit_status(1);
-        return;
+    let mut ap = ArgumentParser::new();
+    ap.set_description("Check a repository for consistency");
+
+    ap.refer(&mut prog_args.respository_path)
+        .add_argument(
+            "repository", box Store::<String>, "Path to a Repository")
+        .required();
+
+    ap.refer(&mut prog_args.deep)
+        .add_option(["-d", "--deep"], box StoreTrue,
+        "Deep scan");
+
+    ap.refer(&mut prog_args.verbose)
+        .add_option(["-v", "--verbose"], box StoreTrue, "Verbose");
+
+    match ap.parse_args() {
+        Ok(()) => {}
+        Err(x) => {
+            os::set_exit_status(x);
+            return;
+        }
     }
 
-    let repository_directory = args_bytes[1].as_slice().clone();
-    let path = Path::new(repository_directory);
+    let path = Path::new(prog_args.respository_path);
 
     // Quick sanity check
     match stat(&path) {
@@ -56,19 +80,26 @@ fn main() {
         Err(e) => fail!("stat error: {}", e)
     }
 
-    let foo = match Repository::load_from_nofsck(&path) {
+    let repo = match Repository::load_from_nofsck(&path) {
         Ok(repo) => repo,
         Err(err) => fail!("Error while reading repository: {}", err)
     };
 
-    let orphans = foo.find_orphans();
+    if prog_args.verbose {
+        println!("Loaded repository with {} nodes", repo.nodes.len());
+    }
+    let orphans = repo.find_orphans();
+
+    if prog_args.verbose && orphans.len() > 0 {
+        println!("    including {} orphans", orphans.len());
+    }
 
     let mut by_uuid: HashMap<Uuid, Vec<BackupNode>> = HashMap::new();
-    for node in foo.nodes.into_iter() {
+    for node in repo.nodes.into_iter() {
         match by_uuid.entry(node.uuid.clone()) {
-            Vacant(entry) => { entry.set(vec![node]); },
-            Occupied(entry) => { entry.into_mut().push(node); }
-        }
+            Vacant(entry) => entry.set(Vec::new()),
+            Occupied(entry) => entry.into_mut()
+        }.push(node);
     }
 
     let mut orphan_nodes_lists = orphans.iter()
@@ -81,4 +112,6 @@ fn main() {
             println!("orphan: {}", orphan_node.path.display());
         }
     }
+
+
 }
